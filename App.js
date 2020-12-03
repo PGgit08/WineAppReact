ffb9c9ccc856abadaebb9d8e1b9f8aa2a14f6d42
PGgit08 <peter.mty8@gmail.com>
2020-12-03 11:19:13 -0500

    Maps added

--- App.js
+++ App.js
@@ -1,47 +1,21 @@
-import React, { Component } from 'react'
-import { View, Text, Button, StyleSheet, } from 'react-native'
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Alert} from "react-native";
+import MapView from "react-native-maps";
 
 class App extends Component {
-  state = {
-    textValue: 'bruh',
-    bottom: 20
-  }
-
-  onPress = () => {
-    var change;
-    if(this.state.textValue == 'boi'){
-      change = 'bruh';
-    }
-
-    else{
-      change = 'boi';
-    };
-
-    this.setState({
-      textValue: change,
-      bottom: this.state.bottom += 1
-    });
-  };
-
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={{color:'yellow', bottom:this.state.bottom}}>{this.state.textValue}</Text>
-        <Button onPress={this.onPress} title="Click Me!"/>
-      </View>
-    )
-  }
+      <MapView
+        style={{
+          flex: 1
+        }}
+        initialRegion={{
+          latitude: 0,
+          longitude: 0
+        }}
+      />
+    );
+  };
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
-    top: 100
-  },
-});
-
-// export default, allows another program to import this function, only this function
 export default App;
